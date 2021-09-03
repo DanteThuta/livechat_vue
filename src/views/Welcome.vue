b6626ba5e0a3af3e41750d7837dbb695fca0d19e
Dante <dante@Dante-MacBook-Air.local>
2021-09-04 00:40:00 +0630

    Creating Welcome Page

---
+++ src/views/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome container">
+      <h2>Welcome</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.container{
+    text-align: center;
+    padding: 20px 0;
+}
+</style>
