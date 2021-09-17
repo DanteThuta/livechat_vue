9a4acf3491d205b8e153de4608789e61a316d13a
Dante <dante@Dante-MacBook-Air.local>
2021-09-17 20:41:57 +0630

    Auto-Scrolling Feature

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chat-window">
-    <div class="messages" >
+    <div class="messages" ref="msgBox">
       <div class="single" v-for="message in formattedMessages" :key="message.id">
           <span class="created_at">{{message.created_at}}</span>
           <span class="name">{{message.name}}</span>
@@ -15,9 +15,16 @@
 import { computed, ref } from '@vue/reactivity'
 import {db} from '../firebase/config'
 import {formatDistanceToNow} from 'date-fns'
+import { onUpdated } from '@vue/runtime-core'
 export default {
     setup(){
       let messages = ref([]);
+      let msgBox = ref(null);
+
+      //Auto-Scrolling Feature
+      onUpdated(()=>{
+        msgBox.value.scrollTop = msgBox.value.scrollHeight;        
+      })
 
       let formattedMessages = computed(()=>{
         return messages.value.map((msg)=>{
@@ -43,7 +50,7 @@ export default {
         // console.log(messages.value);
       })
 
-      return {messages,formattedMessages};
+      return {messages,formattedMessages,msgBox};
     }
 }
 </script>
